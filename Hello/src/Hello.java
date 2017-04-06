05c6e973d26881178c5f1d997cd2db7f3578caad
sjkang <sjkang@daegu.ac.kr>
2017-04-06 10:56:45 +0900

    added 'her world'

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
 		System.out.println("Hello his world");
+		System.out.println("Hello her World");
 
 
 	}
